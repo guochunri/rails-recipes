2752a331470e0a246df2d638bbd48be09971c21c
郭春日 <uochunri@gmail.com>
2017-07-21 23:31:50 +0800

    add description for everyone image

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,9 @@ class Event < ApplicationRecord
   has_many :registrations, :dependent => :destroy
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
+  has_many :attachments, :class_name => "EventAttachment", :dependent => :destroy
+  accepts_nested_attributes_for :attachments, :allow_destroy => true, :reject_if => :all_blank
+
   validates_presence_of :name
   STATUS = ["draft", "public", "private"]
   validates_inclusion_of :status, :in => STATUS

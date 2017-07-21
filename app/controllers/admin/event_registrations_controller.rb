5966676a84b7e8ef760c22d1cb2307d951519212
郭春日 <uochunri@gmail.com>
2017-07-21 22:09:24 +0800

    filter:mutiple select filter

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -4,6 +4,15 @@ class Admin::EventRegistrationsController < AdminController
 
   def index
     @registrations = @event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(10)
+
+    if Array(params[:statuses]).any?
+      @registrations = @registrations.by_status(params[:statuses])
+    end
+
+    if Array(params[:ticket_ids]).any?
+      @registrations = @registrations.by_ticket(params[:ticket_ids])
+    end
+
     if params[:status].present? && Registration::STATUS.include?(params[:status])
      @registrations = @registrations.by_status(params[:status])
     end

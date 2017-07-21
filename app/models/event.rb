dbb78b49143b8397503896c77bdec9e8629ee162
郭春日 <uochunri@gmail.com>
2017-07-21 16:11:05 +0800

    model registration

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
   belongs_to :category, :optional => true
   has_many :tickets, :dependent => :destroy
+  has_many :registrations, :dependent => :destroy
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
   validates_presence_of :name

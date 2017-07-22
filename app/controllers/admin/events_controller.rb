db3c320db13c6470aa32f86d881e14713a70af94
郭春日 <uochunri@gmail.com>
2017-07-22 08:49:21 +0800

    chart:of Amount

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -27,6 +27,16 @@ class Admin::EventsController < AdminController
         }]
     }
 
+     @data2 = {
+         labels: ticket_names,
+         datasets: [{
+             label: '# of Amount',
+             data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+             backgroundColor: colors,
+             borderWidth: 1
+         }]
+     }
+
   end
 
   def new
